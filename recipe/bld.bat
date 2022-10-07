35b97b46acf87472e947643e82422419b3c32672
Silvio Traversaro <silvio@traversaro.it>
2022-10-07 14:53:32 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -27,7 +27,7 @@ cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
 :: Test.
-ctest -C Release -E "INTEGRATION|PERFORMANCE|REGRESSION"
+ctest -C Release -E "INTEGRATION|PERFORMANCE|REGRESSION|UNIT_gz"
 if errorlevel 1 exit 1
 
 :: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
