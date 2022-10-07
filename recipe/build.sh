6a841c0486565a4873c37202ba17cd7df3d9be17
Silvio Traversaro <silvio@traversaro.it>
2022-10-07 16:01:07 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,13 +21,7 @@ sed "s|libgz-tools2-backward.dylib|$PREFIX/lib/libgz-tools2-backward.dylib|" `wh
 cmake ${CMAKE_ARGS} .. \
       -G "Ninja" \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=ON \
-      -DBOOST_ROOT=$PREFIX \
-      -DBoost_NO_SYSTEM_PATHS=ON \
-      -DBoost_NO_BOOST_CMAKE=ON \
-      -DBoost_DEBUG=ON \
       -DUSE_EXTERNAL_TINYXML=ON \
       -DUSE_INTERNAL_URDF=OFF \
       -DBUILD_TESTING=$BUILD_TESTING \
