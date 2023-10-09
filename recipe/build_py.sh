6c0430919f7e5340a2549c330f54d8a48ec3d06e
Silvio Traversaro <silvio@traversaro.it>
2023-10-09 09:24:52 +0200

    Update build_py.sh

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -22,3 +22,7 @@ cmake ${CMAKE_ARGS} -GNinja .. \
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+  ctest --output-on-failure -C Release
+fi
